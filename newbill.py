70e840ff144129cb74443638d4181f52aa23a157
Brady Itkin <brady.itkin@colorado.edu>
2018-02-01 18:00:11 -0700

    Add parsing for existing legislation, background color for legislation_template

--- newbill.py
+++ newbill.py
@@ -7,12 +7,25 @@ import io
 import string
 from xml.sax.saxutils import escape
 
-#Things TODO:
-#Modularize Bill so that the class can be more inheritable
-#Create a working way to parse "Actions" and votes
+#Things TODO for Bill class:
+#Create a working way to parse "Actions" and votes -- Focus is shifting to a manual way to do data entry gracefully, as OCR for sessions <83 does not lend a graceful way to process this. 
 #Separate names from positions
-#Get "Authors" to stop glitching
-#Sessions working out of box (minus TODOS and introducedDate): 83-86
+#Get "Authors" to stop glitching -- Focus is also shifting to a manual way to do data entry gracefully here as well.
+#Sessions that were working out of box (minus introducedDate): 83-86 -- now we should verify that recent changes in the past month and a half haven't broken compatibility. New driver should be written regardless
+
+class Sponsor:
+	name=""
+	title=""	
+	def __init__(self,sponsorName,sponsorTitle):
+		self.name=sponsorName
+		self.title=sponsorTitle
+	def createElement():
+		root=ET.Element("item")
+		eName=ET.SubElement(root,"name")
+		eName.text=self.name
+		eTitle=ET.SubElement(root,"title")
+		eTitle.text=self.title
+		return root
 
 class Bill:
 	rawinputstr="" #Non ASCIIFied
@@ -30,6 +43,7 @@ class Bill:
 	sponsors=[]
 	authors=[]
 	actions=[]
+	tree=None
 	preAudit=False
 	#####Regular Expression search objects Used To Denote Document Structure For Parsing#####
 	#####Initialized in self.parseFile()#####
@@ -49,19 +63,24 @@ class Bill:
 	SPONSORSMATCHRE="Sponsor.*:.*"
 	SUBHEADERMATCHRE="(Legislative Council|A Resolution|A Bill)"
 	ACTIONSMATCHRE="^(\s\S)+(-)+(\s)*(PASSES|FAILS|POSTPONED|.*REFER.*|.*RATIF.*)(\s)*(-)+(\s)*(EXECUTIVE|LEGISLATIVE)(\s)*(COUNCIL)(\s\S)+$" #This needs some srs ironing out, may be better to do this crowdsourced or some type of fuzzy string matching
-	def __init__(self,infile):
-		with io.open(infile,'r',encoding='utf-8') as f:
-			self.rawinputStr=f.read()
-		self.tempBillString=os.path.splitext(infile)[0]
-		billNameInfo=self.tempBillString.split('_')
-		self.billSession=int(billNameInfo[0])
-		self.billType=billNameInfo[1]
-		self.billNumber=billNameInfo[2]
+	def __init__(self,infile,xmlcreated=False):
+		if (xmlcreated):
+			self.parseXML(infile)
+			#We trust the XML more than the filenames in this case. There should be some intermediary in between this call and validating the XML
+			#Which, speaking of, #TODO create an XML schema in DTD or XSD.
+		else:		
+			with io.open(infile,'r',encoding='utf-8') as f:
+				self.rawinputStr=f.read()
+			self.tempBillString=os.path.splitext(infile)[0]
+			billNameInfo=self.tempBillString.split('_')
+			self.billSession=int(billNameInfo[0])
+			self.billType=billNameInfo[1]
+			self.billNumber=billNameInfo[2]
 	def sanitizeInputStr(self):
 		#try:
 		#	self.inputStr=self.rawinputStr.encode('utf-8','xmlcharrefreplace')
 		#except:
-		self.rawinputStr=escape(self.rawinputStr)
+		self.rawinputStr=escape(self.rawinputStr) #Is this all the safest way possible? Acrobat OCR is weird, maybe tesseract will lend itself to a better solution at the OCR level.
 		tempArray=bytearray(self.rawinputStr,"utf-8")
 		for x in tempArray:
 			if (x>31 and x<128) or x==10:
@@ -72,8 +91,8 @@ class Bill:
 		#		self.inputStr+=x
 	def parseFile(self):
 		#TODO: actions
-		self.historyMatch=re.search(self.HISTORYMATCHRE,self.inputStr, flags=re.I) #possibly resolutions, case insensitive
-		self.summaryMatch=re.search(self.SUMMARYMATCHRE,self.inputStr, flags=re.I) #possibly resolutions, case insensitive
+		self.historyMatch=re.search(self.HISTORYMATCHRE,self.inputStr, flags=re.I) #NOTE: early resolutions do not have these, case insensitive
+		self.summaryMatch=re.search(self.SUMMARYMATCHRE,self.inputStr, flags=re.I) #NOTE: early resolutions do not have these, case insensitive
 		self.beginbillMatch=re.search(self.BEGINBILLMATCHRE,self.inputStr,flags=re.I) #good type of lazy
 		if (self.beginbillMatch is None):
 			self.beginbillMatch=re.search(self.BEGINBILLMATCHALTRE,self.inputStr,flags=re.I) # It will still have this phrase if it doesn't have preambulatories. Usually applies to early bills
@@ -81,8 +100,8 @@ class Bill:
 		if (self.endbillMatch is None):
 			self.preAudit=True #vote counts and details of action didn't happen necessarily happen pre-audit for legislation after the millenium
 
-		self.authorsMatch=re.search(self.AUTHORSMATCHRE,self.inputStr) #bad type oflazy
-		self.sponsorsMatch=re.search(self.SPONSORSMATCHRE,self.inputStr) #bad type of lazy
+		self.authorsMatch=re.search(self.AUTHORSMATCHRE,self.inputStr) #bad type of lazy -- considering getting rid of this in newbill and shifting to session drivers.
+		self.sponsorsMatch=re.search(self.SPONSORSMATCHRE,self.inputStr) #bad type of lazy -- considering getting rid of this in newbill and shifting to session drivers.
 		self.subheaderMatch=re.search(self.SUBHEADERMATCHRE,self.inputStr,flags=re.I) #lazy?
 		
 		
@@ -155,10 +174,31 @@ class Bill:
 			self.sponsors=self.sponsorsString.splitlines()
 			self.authors=self.authorsString.splitlines()
 			self.sponsors=filter(None,self.sponsors)
-			self.authors=filter(None,self.authors)			 
+			self.authors=filter(None,self.authors)
+	def parseXML(self,infile):
+		self.tree=ET.parse(infile)
+		root=self.tree.getroot()
+		prefix="./bill/"
+		eBillNum=root.find(prefix+'billNumber')
+		eBillType=root.find(prefix+'billType')
+		eBillSess=root.find(prefix+'sessionNumber')
+		eBillText=root.find(prefix+'billText')
+		#eSummaries=root.find(prefix+'summaries')
+		eBillSummary=root.find(prefix+"summaries/summary")
+		eBillHistory=root.find(prefix+'summaries/history')
+		eBillIntroducedDate=root.find(prefix+'introducedDate')
+		
+		self.introducedDate=eBillIntroducedDate.text
+		self.billNumber=int(eBillNum.text)
+		self.billType=eBillType.text
+		self.billSession=int(eBillSess.text)
+		self.billHistoryString=eBillHistory.text
+		self.billSummaryString=eBillSummary.text
+		self.billText=eBillText.text
+		#TODO: parse sponsors, authors, and actions.	 
 	def createXML(self):
-		tree=ET.parse('base.xml')
-		root=tree.getroot()
+		self.tree=ET.parse('base.xml')
+		root=self.tree.getroot()
 		prefix='./bill/'
 		eBillNum=root.find(prefix+'billNumber')
 		eBillType=root.find(prefix+'billType')
@@ -179,24 +219,26 @@ class Bill:
 
 		self.addSponsorsAndAuthorsXML(root)
 		self.addActions(root)
-		tree.write("{}_{}_{}.xml".format(self.billSession,self.billType,self.billNumber),encoding='utf-8',xml_declaration=True)
+		self.tree.write("{}_{}_{}.xml".format(self.billSession,self.billType,self.billNumber),encoding='utf-8',xml_declaration=True)
 	def addSponsorsAndAuthorsXML(self,root):
 		#TODO: titles, differentiation between sponsors and authors
-		eBillSponsors=root.find('./bill/sponsors')
-		currentItemElement=None
-		for sponsor in self.sponsors+self.authors:
-			currentItemElement=ET.SubElement(eBillSponsors,'item')
-			ET.SubElement(currentItemElement,'name').text=sponsor
+		#eBillSponsors=root.find('./bill/sponsors')
+		#currentItemElement=None
+		#for sponsor in self.sponsors+self.authors:
+		#	currentItemElement=ET.SubElement(eBillSponsors,'item')
+		#	ET.SubElement(currentItemElement,'name').text=sponsor
+		pass
 	def addActions(self,root):
-		eBillActions=root.find('./bill/actions')
-		eBillLatestAction=root.find('./bill/latestAction')
-		currentItemElement=None		
-		for x in self.actions:
-			currentItemElement=ET.SubElement(eBillActions,'item')
-			currentItemElement.text=x
-		if currentItemElement is not None:		
-			eBillLatestAction.text=currentItemElement.text
-
-
+		#TODO		
+		#eBillActions=root.find('./bill/actions')
+		#eBillLatestAction=root.find('./bill/latestAction')
+		#currentItemElement=None		
+		#for x in self.actions:
+		#	currentItemElement=ET.SubElement(eBillActions,'item')
+		#	currentItemElement.text=x
+		#if currentItemElement is not None:		
+		#	eBillLatestAction.text=currentItemElement.text
+		pass
+		
 
 
